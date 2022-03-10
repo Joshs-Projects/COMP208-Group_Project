c502ad4c0edde0244e7ad8cdd2514261ae2b0862
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2022-03-10 14:30:12 +0000

    Added a red box using Jpanel to check objects can be drawn on the screen.

--- src/RendererTester.java
+++ src/RendererTester.java
@@ -5,7 +5,10 @@ public class RendererTester {
     public RendererTester(){
         Renderer display = new Renderer(1200, 720, "Testing");
 
+        //Draw function allowing the user to draw x y size image image to display, user puts in an object.
+
         display.DrawWindow();
+        display.DrawObject();
 
         return;
     }
@@ -13,4 +16,6 @@ public class RendererTester {
     public static void main(String args[]){
         RendererTester test = new RendererTester();
     }
+
+
 }
