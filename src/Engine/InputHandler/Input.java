a16e6be5852aea48d5ccf2ea24e01e4b41e30804
DanD0307 <dannydineen2002@gmail.com>
2022-03-10 14:18:02 +0000

    Basic skeleton code of the Input Class

--- src/Engine/InputHandler/Input.java
+++ src/Engine/InputHandler/Input.java
@@ -1,4 +1,23 @@
 package Engine.InputHandler;
 
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
 public class Input {
+
+
+    private ArrayList<Integer> keysPressed = new ArrayList<Integer>();
+    private ArrayList<Integer> keysDown = new ArrayList<Integer>();
+
+    private int mouseX, mouseY;
+    private int scroll;
+
+    public void keyReleased(KeyEvent event) {
+        int key = event.getKeyCode();
+    }
+
+    public void keyPressed(KeyEvent event) {
+        int key = event.getKeyCode();
+    }
 }
