671b8fe9a4b30dc9185eebbfb3f7bcd1d5c3d3c1
jamessaunders <sauders2433@gmail.com>
2022-03-10 14:18:17 +0000

    working on entity, created defult constructor

--- src/Engine/Physics/EntityPhysics.java
+++ src/Engine/Physics/EntityPhysics.java
@@ -1,6 +1,6 @@
 package Engine.Physics;
 
-public class EntityPhysics extends Movement{
+public class EntityPhysics extends Movement {
     //Create a bool for is the entity movable.
     //Create a constructor that takes in: (Most of this can be found in the CharacterPhysics)
     //Size
@@ -9,7 +9,13 @@ public class EntityPhysics extends Movement{
     //Mass
     //Make setters and getters for all relevant attributes.
     //THIS EXCLUDES ATTRIBUTES THAT WERE INHERITED AS THEY SHOULD HAVE PRE-EXISTING SETTERS AND GETTERS
+    protected boolean movable;
 
 
+    //default Construtor
+    public EntityPhysics() {
+    }
+
+    //2nd construtor
 
 }
