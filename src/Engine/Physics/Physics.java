97d51abe5691cfaf7e4603ba9b944e72b06ce306
Joshs-Projects <74363804+Joshs-Projects@users.noreply.github.com>
2022-03-30 14:09:41 +0100

    Added JUnit Testing files.

--- src/Engine/Physics/Physics.java
+++ src/Engine/Physics/Physics.java
@@ -11,42 +11,92 @@ public class Physics {
     protected double xSize;
     protected double ySize;
 
+    /**
+     * Gets the gravity attribute
+     *
+     * @return double of gravity value
+     */
     public double getGravity() {
         return gravity;
     }
 
+    /**
+     * Sets the gravity attribute.
+     *
+     * @param gravity A double representing the strength of gravity
+     */
     public void setGravity(double gravity) {
         this.gravity = gravity;
     }
 
+    /**
+     * Gets the timePerTick attribute
+     *
+     * @return int of timePerTick
+     */
     public int getTimePerTick() {
         return timePerTick;
     }
 
+    /**
+     * Sets the timePerTick attribute
+     *
+     * @param timePerTick An int representing the amount of time that will elapse per update
+     */
     public void setTimePerTick(int timePerTick) {
         this.timePerTick = timePerTick;
     }
 
+    /**
+     * Gets the xPos attribute
+     *
+     * @return double of xPos
+     */
     public double getxPos() {
         return xPos;
     }
 
+    /**
+     * Sets the xPos attribute
+     *
+     * @param xPos A double representing the horizontal position
+     */
     public void setxPos(double xPos) {
         this.xPos = xPos;
     }
 
+    /**
+     * Gets the yPos attribute
+     *
+     * @return double of yPos
+     */
     public double getyPos() {
         return yPos;
     }
 
+    /**
+     * Sets the yPos attribute
+     *
+     * @param yPos A double representing the horizontal position
+     */
     public void setyPos(double yPos) {
         this.yPos = yPos;
     }
 
+    /**
+     * Gets the xSize attribute
+     *
+     * @return double of xSize
+     */
     public double getxSize() {
         return xSize;
     }
 
+    /**
+     * Sets the xSize attribute
+     *
+     * @param xSize A double representing the horizontal position
+     */
     public void setxSize(double xSize) {
         this.xSize = xSize;
     }
@@ -55,6 +105,11 @@ public class Physics {
         return ySize;
     }
 
+    /**
+     * Sets the ySize attribute
+     *
+     * @param ySize A double representing the horizontal position
+     */
     public void setySize(double ySize) {
         this.ySize = ySize;
     }
---
+++ src/Testing/Physics/MovementUnitTest.java
@@ -0,0 +1,85 @@
+package Physics;
+
+import Engine.Physics.Movement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MovementTest {
+    private static Movement movementTestingObject;
+
+    Queue<Double> xVelocity = new LinkedList<>();
+    Queue<Double> yVelocity = new LinkedList<>();
+    Queue<Double> xAcceleration = new LinkedList<>();
+    Queue<Double> yAcceleration = new LinkedList<>();
+    Queue<Double> mass = new LinkedList<>();
+
+    //Test Data Generation
+    int numberOfTests = 10;
+    double velocityMin = -10000, velocityMax = 10000;
+    double accelerationMin = -10000, accelerationMax = 10000;
+    double massMin = 0, massMax = 10000;
+
+
+    @BeforeAll
+    public static void setup(){
+        movementTestingObject = new Movement(2,4,2,4,1);
+    }
+
+    @BeforeEach
+    public void generateTestData(){
+        Random r = new Random();
+        for (int i = 0; i < numberOfTests; i++) {
+            xVelocity.add(velocityMin + (velocityMax - velocityMin) * r.nextDouble());
+            yVelocity.add(velocityMin + (velocityMax - velocityMin) * r.nextDouble());
+            xAcceleration.add(accelerationMin + (accelerationMax - accelerationMax) * r.nextDouble());
+            yAcceleration.add(accelerationMin + (accelerationMax - accelerationMin) * r.nextDouble());
+            mass.add(massMin + (massMax - massMin) * r.nextDouble());
+        }
+    }
+
+    @Test
+    public void testTheConstructor(){
+        assertEquals(2, movementTestingObject.getxVelocity());
+        assertEquals(4, movementTestingObject.getyVelocity());
+        assertEquals(2, movementTestingObject.getxAcceleration());
+        assertEquals(4, movementTestingObject.getyAcceleration());
+        assertEquals(1, movementTestingObject.getMass());
+    }
+
+    @Test
+    public void testTheSettersAndGetters(){
+        double xVelocityTestValue;
+        double yVelocityTestValue;
+        double xAccelerationTestValue;
+        double yAccelerationTestValue;
+        double massTestValue;
+        for (int i = 0; i < numberOfTests; i++) {
+            xVelocityTestValue = xVelocity.remove();
+            yVelocityTestValue = yVelocity.remove();
+            xAccelerationTestValue = xAcceleration.remove();
+            yAccelerationTestValue = yAcceleration.remove();
+            massTestValue = mass.remove();
+
+            movementTestingObject.setxVelocity(xVelocityTestValue);
+            movementTestingObject.setyVelocity(yVelocityTestValue);
+            movementTestingObject.setxAcceleration(xAccelerationTestValue);
+            movementTestingObject.setyAcceleration(yAccelerationTestValue);
+            movementTestingObject.setMass(massTestValue);
+
+            assertEquals(xVelocityTestValue, movementTestingObject.getxVelocity());
+            assertEquals(yVelocityTestValue, movementTestingObject.getyVelocity());
+            assertEquals(xAccelerationTestValue, movementTestingObject.getxAcceleration());
+            assertEquals(yAccelerationTestValue, movementTestingObject.getyAcceleration());
+            assertEquals(massTestValue, movementTestingObject.getMass());
+        }
+    }
+
+
+}
