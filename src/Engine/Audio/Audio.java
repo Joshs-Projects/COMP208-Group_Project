801de173ee6e9fb023fbbcf5ae5048a6bf789c78
bleizthomas <bleiiizthomas@gmail.com>
2022-04-05 15:50:04 +0100

    Added foundations for playing and manipulating audio files

--- src/Engine/Audio/Audio.java
+++ src/Engine/Audio/Audio.java
@@ -1,4 +1,56 @@
 package Engine.Audio;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+
+// throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException
+
 public class Audio {
+    public static void main(String[] args) {
+        // Instantiates itself
+        Audio audio = new Audio();
+        File file = new File("src/Engine/Audio/ThinkBreak.wav"); // just an arbitrary sample for now
+        audio.playAudio(file);
+    }
+
+    public void playAudio(File file) {
+        try {
+            // takes the audio input in from the specified file
+            AudioInputStream lineIn = AudioSystem.getAudioInputStream(file);
+            AudioFormat outputFormat = getOutputFormat(lineIn.getFormat());
+            // allows for the writing of audio data
+            SourceDataLine lineOut = AudioSystem.getSourceDataLine(outputFormat);
+            int buffSize = 4096;
+            lineOut.open(outputFormat, buffSize);
+            lineOut.start();
+            // writes audio data to lineOut, sample by sample
+            byte[] buffer = new byte[buffSize];
+            for (int i = 0; i != -1; i = lineIn.read(buffer, 0, buffSize)) {
+                lineOut.write(buffer, 0, i);
+            }
+            // System.out.println("working?");
+            // closes output line
+            lineOut.drain();
+            lineOut.stop();
+            lineOut.close();
+        }
+        catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private AudioFormat getOutputFormat(AudioFormat lineInFormat) {
+        int outChannels = 2; // 1 if mono
+        return new AudioFormat(
+                PCM_SIGNED,
+                lineInFormat.getSampleRate(),
+                16,
+                outChannels,
+                lineInFormat.getFrameSize(),
+                lineInFormat.getSampleRate(),
+                false
+        );
+    }
 }
---
+++ src/Testing/Engine/Audio/AudioTest.java
@@ -0,0 +1,18 @@
+package Engine.Audio;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AudioTest {
+    // To generate tests/do something before the tests are actually carried out (for some test X)
+    @BeforeEach
+    public void BeforeXTest() {
+    }
+    @Test
+    public void Placeholder() {
+        assertEquals(1, 1);
+    }
+}
