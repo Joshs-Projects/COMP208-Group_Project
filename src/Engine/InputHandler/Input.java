9e6cdfc511acb6650efb9da6b5356b9ed0427d6f
DanD0307 <dannydineen2002@gmail.com>
2022-04-11 16:08:05 +0100

    Skeleton Code further developed

--- src/Engine/InputHandler/Input.java
+++ src/Engine/InputHandler/Input.java
@@ -1,12 +1,12 @@
 package Engine.InputHandler;
 
 
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 
-public class Input {
+public class Input implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener {
 
-
+    private final int keyAmount = 256;
     private ArrayList<Integer> keysPressed = new ArrayList<Integer>();
     private ArrayList<Integer> keysDown = new ArrayList<Integer>();
 
@@ -17,7 +17,52 @@ public class Input {
         int key = event.getKeyCode();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
     public void keyPressed(KeyEvent event) {
         int key = event.getKeyCode();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+
     }
 }
