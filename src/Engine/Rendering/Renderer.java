9f63a5c6e794e61ff43b6cd146697ddabf8c5905
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2022-04-12 10:42:42 +0100

    Reworked the renderer from scratch, code is now simpler and less fiddly. Current functionality the user can draw windows and add images to them.

--- src/Engine/Rendering/Renderer.java
+++ src/Engine/Rendering/Renderer.java
@@ -1,30 +1,15 @@
 package Engine.Rendering;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.util.ArrayList;
-
 
 public class Renderer {
-    private int xResolution;
-    private int yResolution;
+    private final int xResolution;
+    private final int yResolution;
+    private final String windowTitle;
 
-    private String color;
-    private int objectX;
-    private int objectY;
-    private int objectH;
-    private int objectW;
-    private String path;
-
-    private String windowTitle;
-
-    //Object gameObject;
-    //overall frame
     private JFrame frame;
-
-    //THINGS TO DRAW. MAYBE NEEDED MAYBE NOT.
-    //list of assests
-    ArrayList<Assets> ItemsToRender = new ArrayList<Assets>();
 
     public Renderer(int xResolution, int yResolution, String windowTitle) {
         this.xResolution = xResolution;
@@ -32,70 +17,29 @@ public class Renderer {
         this.windowTitle = windowTitle;
     }
 
-    public JFrame drawWindow() {
+    public void Window() {
+
         frame = new JFrame(windowTitle);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(xResolution, yResolution);
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(null);
+
+    }
+
+    public void addImage(int xPosition, int yPosition, int width, int height){
+
+        ImageIcon image = new ImageIcon("stingRay.png");
+
+        Border border = BorderFactory.createLineBorder(Color.red,3);
+
+        JLabel label = new JLabel();
+
+        label.setBounds(xPosition,yPosition,width,height);
+        label.setIcon(image);
+        label.setBorder(border);
+
         frame.setVisible(true);
-
-        return frame;
-
-    }
-
-    public void DrawObject(int ObjectX,int ObjectY,int ObjectH,int ObjectW){
-        JPanel panel = new JPanel();
-        panel.setBounds(40,80,200,200);
-        //default colour
-        panel.setBackground(Color.black);
-        frame.add(panel);
-    }
-
-    //With optional parameter of color
-    public void DrawObject(int ObjectX,int ObjectY,int ObjectH,int ObjectW, Color color){
-        JPanel panel = new JPanel();
-        panel.setBounds(40,80,200,200);
-        panel.setBackground(color);
-        frame.add(panel);
-    }
-
-    public void DrawObject(int ObjectX,int ObjectY,int ObjectH,int ObjectW,String path){
-        /* if the actual height of the image > the actual width, and the user scales the image with a greater
-        width (Image.SCALE_SMOOTH) will rotate to prevent distortion. Could prevent the user from doing this entirely
-        or make sure the width/height is always the correct way around even if incorrectly specified by the user.
-
-        Have the user decide what level of intensity they want the image to use up the cpu
-        by typing an integer, this will correspond with a different use of (Image.(different types of scaling))
-        */
-        ImageIcon imageIcon = new ImageIcon (path);
-        Image image = imageIcon.getImage();
-        Image scaledImage = image.getScaledInstance(400, 250, Image.SCALE_SMOOTH);
-        imageIcon = new ImageIcon(scaledImage);
-
-        JLabel label = new JLabel(imageIcon);
-
-        //label.setBounds(40,80,200,200);
-        //label.setLocation(40, 40);
         frame.add(label);
-        //frame.setSize(480, 480);
-        frame.setVisible(true);
+
     }
 }
-
-    //THIS IS TO TRY TO DRAW A THING ON SCREEN
-    /*
-    public void DrawAssets(Graphics g){
-        g.drawImage(ItemsToRender.get(0).getImage(), 32, 32, frame);
-        g.fillRect(50, 50, 1000, 100);
-    }
-    */
-    /*
-    public void AddThingToRender(Assets newThing) throws RuntimeException {
-        ItemsToRender.add(newThing);
-
-        DrawAssets(frame.getGraphics());
-
-    }
-    */
-
-    //THIS IS THE END OF TRYING TO DRAW STUFF ON SCREEN
--- src/RendererTester.java
+++ src/RendererTester.java
@@ -5,18 +5,11 @@ import java.awt.*;
 public class RendererTester {
 
     public RendererTester(){
-        Renderer display = new Renderer(1200, 720, "Testing");
-        //ObjectDrawer object  = new ObjectDrawer("red");
+        Renderer display = new Renderer(1200, 720, "A New Game");
 
-        //Draw function allowing the user to draw x y size image image to display, user puts in an object.
+        display.Window();
+        display.addImage(20,0, 256,256);
 
-        display.drawWindow();
-
-        //display.DrawObject(20,20,40,40,Color.cyan);
-
-        display.DrawObject(20,20,40,40,"C:\\Users\\Josh\\IdeaProjects\\COMP208-Group_Project\\src\\Engine\\Rendering\\stingRay.jpg");
-
-        return;
     }
 
     public static void main(String args[]){
